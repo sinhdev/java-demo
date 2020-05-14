d8937077cbabff9e8e659336719c2c47e802a522
Sinh NX <sinhnx@Sinhs-MacBook-Pro.local>
2020-05-14 23:37:10 +0700

    update Aes256Algorithms

--- JavaSecurities/Aes256Algorithms.java
+++ JavaSecurities/Aes256Algorithms.java
@@ -3,16 +3,21 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Base64;
+import java.security.spec.KeySpec;
 
 import javax.crypto.Cipher;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.spec.IvParameterSpec;
 
 public class Aes256Algorithms {
     public static void main(final String[] args) {
         final String secretKey = "sinhnx.dev";
-        String originalMessage = "Cộng hòa xã hội chủ nghĩa Việt Nam.";
-        String encryptedMessage = AesAlgorithms.encrypt(originalMessage, secretKey);
-        String decryptedMessage = AesAlgorithms.decrypt(encryptedMessage, secretKey);
+        final String originalMessage = "Cộng hòa xã hội chủ nghĩa Việt Nam.";
+        final String encryptedMessage = Aes256Algorithms.encrypt(originalMessage, secretKey);
+        final String decryptedMessage = Aes256Algorithms.decrypt(encryptedMessage, secretKey);
 
         System.out.println("Original Message: " + originalMessage);
         System.out.println("Encrypted Message: " + encryptedMessage);
@@ -21,30 +26,29 @@ public class Aes256Algorithms {
 
     private static String salt = "https://github.com/sinhdev";
 
-    public static String decrypt(String strToDecrypt, String secret) {
+    public static String decrypt(final String strToDecrypt, final String secret) {
         try {
-            byte[] iv = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-            IvParameterSpec ivspec = new IvParameterSpec(iv);
+            final byte[] iv = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            final IvParameterSpec ivspec = new IvParameterSpec(iv);
 
-            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-            KeySpec spec = new PBEKeySpec(secret.toCharArray(), salt.getBytes(), 65536, 256);
-            SecretKey tmp = factory.generateSecret(spec);
-            SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+            final SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            final KeySpec spec = new PBEKeySpec(secret.toCharArray(), salt.getBytes(), 65536, 256);
+            final SecretKey tmp = factory.generateSecret(spec);
+            final SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
 
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            final Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
             cipher.init(Cipher.DECRYPT_MODE, secretKey, ivspec);
             return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
-        } catch (Exception e) {
+        } catch (final Exception e) {
             System.out.println("Error while decrypting: " + e.toString());
         }
         return null;
     }
 
-    public static String encrypt(String strToEncrypt, String secret) {
+    public static String encrypt(final String strToEncrypt, final String secret) {
         try {
             byte[] iv = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
             IvParameterSpec ivspec = new IvParameterSpec(iv);
-
             SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
             KeySpec spec = new PBEKeySpec(secret.toCharArray(), salt.getBytes(), 65536, 256);
             SecretKey tmp = factory.generateSecret(spec);
@@ -53,7 +57,7 @@ public class Aes256Algorithms {
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
             cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivspec);
             return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
-        } catch (Exception e) {
+        } catch (final Exception e) {
             System.out.println("Error while encrypting: " + e.toString());
         }
         return null;
